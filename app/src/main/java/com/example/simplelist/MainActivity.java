195aa959b3b894a3e22fe62c9fdc81e89d102afe
serg0 <serg.01@bk.ru>
2020-01-21 16:27:30 +0300

    2 home work

--- app/src/main/java/com/example/simplelist/MainActivity.java
+++ app/src/main/java/com/example/simplelist/MainActivity.java
@@ -3,27 +3,28 @@ package com.example.simplelist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
+    SharedPreferences sharedPreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.zone_list);
-        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        final ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
         HashMap<String, String> map;
         String Name = "Name";
         String Description = "Description";
@@ -38,10 +39,34 @@ public class MainActivity extends AppCompatActivity {
         map.put(Description,"А ты все равно уже заплатил,да и курс к концу подходит");
         arrayList.add(map);
 
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(Name,"Сергей");
+        editor.putString(Description,"Почему я сам должен искать инфу ?");
+        editor.putString(Name,"Андрей");
+        editor.putString(Description,"А ты все равно уже заплатил,да и курс к концу подходит");
+        editor.apply();
 
-        SimpleAdapter adapter = new SimpleAdapter(this ,arrayList , R.layout.simple_list ,
+
+        final SimpleAdapter adapter = new SimpleAdapter(this ,arrayList , R.layout.simple_list ,
                 new String[]{Name,Description},
                 new int[]{R.id.firstTxt,R.id.secondTxt});
         listView.setAdapter(adapter);
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                arrayList.remove(position);
+            }
+        });
+        final SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener (){
+            @Override
+            public void onRefresh(){
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
