0faa2851a7cec2ebe0efec7c0f20ca9a36661d03
serg0 <serg.01@bk.ru>
2020-01-24 19:55:25 +0300

    Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'android.content.res.Resources android.content.Context.getResources()' on a null object reference

--- app/src/main/java/com/example/simplelist/MainActivity.java
+++ app/src/main/java/com/example/simplelist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simplelist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -8,6 +9,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -17,56 +20,65 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     SharedPreferences sharedPreferences;
-    public static final String MyPREFERENCES = "MyPrefs" ;
-
+    public static final String MY_PREFERENCES = "MyPrefs";
+    SwipeRefreshLayout swipeLayout;
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+    SimpleAdapter adapter;
+    HashMap<String, String> map;
+    String[] values = prepareContent();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.zone_list);
-        final ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
-        HashMap<String, String> map;
-        String Name = "Name";
-        String Description = "Description";
+        initSharedPreferences();
 
         map = new HashMap<>();
-        map.put(Name,"Сергей");
-        map.put(Description,"Почему я сам должен искать инфу ?");
-        arrayList.add(map);
-
-        map = new HashMap<>();
-        map.put(Name,"Андрей");
-        map.put(Description,"А ты все равно уже заплатил,да и курс к концу подходит");
-        arrayList.add(map);
-
-        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(Name,"Сергей");
-        editor.putString(Description,"Почему я сам должен искать инфу ?");
-        editor.putString(Name,"Андрей");
-        editor.putString(Description,"А ты все равно уже заплатил,да и курс к концу подходит");
-        editor.apply();
+        for (int i = 0; i < prepareContent().length; i++) {
+            String string = String.valueOf(i);
+            map.put(string, prepareContent()[i]);
+            arrayList.add(map);
+        }
 
 
-        final SimpleAdapter adapter = new SimpleAdapter(this ,arrayList , R.layout.simple_list ,
-                new String[]{Name,Description},
-                new int[]{R.id.firstTxt,R.id.secondTxt});
-        listView.setAdapter(adapter);
-
+        BaseAdapter listContentAdapter = createAdapter(values);
+        listView.setAdapter(listContentAdapter);
 
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 arrayList.remove(position);
-            }
-        });
-        final SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
-        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener (){
-            @Override
-            public void onRefresh(){
                 adapter.notifyDataSetChanged();
             }
         });
+        swipeLayout = findViewById(R.id.swiperefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeLayout.setRefreshing(false);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void initSharedPreferences() {
+        sharedPreferences = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
+        for (int i = 0; i < prepareContent().length; i++) {
+            String string = String.valueOf(i);
+            sharedPreferences.edit()
+                    .putString(string, prepareContent()[i]);
+        }
+    }
+
+    @NonNull
+    private BaseAdapter createAdapter(String[] values) {
+        return new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+    }
+
+    @NonNull
+    private String[] prepareContent() {
+        return getString(R.string.large_text).split("\n\n");
     }
 }
